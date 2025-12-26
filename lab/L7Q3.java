d51271dd0a452d13f7ada274adb004962cf888d0
JinRu1121 <23116192@siswa.um.edu.my>
2025-12-26 11:17:05 +0800

    done q3

---
+++ lab/L7Q3.java
@@ -0,0 +1,24 @@
+package lab;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class L7Q3 {
+    public static void main(String[] args) {
+        try {
+            Scanner read = new Scanner(new FileInputStream("input.txt"));
+            PrintWriter write = new PrintWriter(new FileOutputStream("reverse.txt"));
+
+            while (read.hasNextLine()) {
+                String line = read.nextLine();
+                // Reverse the string
+                StringBuilder reversed = new StringBuilder(line).reverse();
+                write.println(reversed.toString());
+            }
+            read.close();
+            write.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Input file not found.");
+        }
+    }
+}
